8d3d7cd450ef3c3468b50a07500b4a893f3589a8
Erin Lipman <erlipman81@gmail.com>
2018-11-14 17:17:43 -0600

    code to generate overlay plots

---
+++ share/overlay_heatmaps.R
@@ -0,0 +1,11 @@
+overlay <- function(fpr, fnr, ppv, thresh=.01){
+  stopifnot(dim(fpr)==dim(fnr))
+  stopifnot(dim(fnr)==dim(ppv))
+  ppv[is.na(ppv)]=1
+  
+  mat = matrix(0, nrow=nrow(fpr), ncol=ncol(fpr))
+  mat[abs(fpr)<=thresh]=mat[abs(fpr)<=thresh]+.4
+  mat[abs(fnr)<=thresh]=mat[abs(fnr)<=thresh]+.3
+  mat[abs(ppv)<=thresh]=mat[abs(ppv)<=thresh]+.2
+  image(mat, col = grey(seq(1, 0, length = 256)))
+}
