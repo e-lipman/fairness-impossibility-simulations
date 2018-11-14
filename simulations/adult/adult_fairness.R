d4d241100ed83d33fd48084bba23c1b90ce5dfa0
Erin Lipman <erlipman81@gmail.com>
2018-11-14 17:55:45 -0600

    overlays for adult data

--- simulations/adult/adult_fairness.R
+++ simulations/adult/adult_fairness.R
@@ -1,4 +1,7 @@
 source("../../share/make_heatmap.R")
+source("../../share/overlay_heatmaps.R")
+
+library(randomForest)
 
 # setup and model
 adult = read.csv("adult.txt")
@@ -14,5 +17,10 @@ probs_rf <- predict(rf, type="prob")[,2]
 lmod = glm(X50K~State.gov+Bachelors+Never.married+Adm.clerical+White+Male, data=adult, family="binomial")
 probs_lm = predict(lmod,type="response")
 
-make_heatmap(adult$X50K, probs_rf, adult$Male)
-make_heatmap(adult$X50K, probs_lm, adult$Male)
+# make heatmaps
+rf_metrics = make_heatmap(adult$X50K, probs_rf, adult$Male)
+lm_metrics = make_heatmap(adult$X50K, probs_lm, adult$Male)
+
+# overlay heatmaps
+overlay(rf_metrics$fpr, rf_metrics$fnr, rf_metrics$ppv,)
+overlay(lm_metrics$fpr, lm_metrics$fnr, lm_metrics$ppv, thresh=.01)
